0e987967dc6a2f031d40ea5943129e53ccd97ff6
Thijs :) <302275953@student.rocmondriaan.nl>
2024-04-23 15:05:10 +0200

    login logout register

---
+++ src/Controller/AdventureController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdventureController extends AbstractController
+{
+    #[Route('/', name: 'main')]
+    public function index(): Response
+    {
+        return $this->render('adventure/index.html.twig', [
+            'controller_name' => 'AdventureController',
+        ]);
+    }
+
+
+
+
+}
+
